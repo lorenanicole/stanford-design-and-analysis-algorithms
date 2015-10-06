50f12887c7e22fb3457625763da07d6e5af8cb24
Lorena Mesa <me@lorenamesa.com>
2015-10-06 13:07:48 -0500

    Add .gitignore, Week-1 module

---
+++ src/WeekOne/KaratsubaMultiplication.java
@@ -0,0 +1,10 @@
+package WeekOne;
+
+/**
+ * Created by lorenamesa on 10/6/15.
+ */
+public class KaratsubaMultiplication {
+    public static void main(String[] args) {
+        System.out.println("WEeee!");
+    }
+}
